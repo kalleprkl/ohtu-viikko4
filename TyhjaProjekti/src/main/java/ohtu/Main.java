b31b2d2f4c3c9166f6910f409170d46b8b9dba26
Kalle Perkoila <kalle.perkoila@helsinki.fi>
2017-04-09 11:28:37 +0300

    tehtävä3

--- TyhjaProjekti/src/main/java/ohtu/Main.java
+++ TyhjaProjekti/src/main/java/ohtu/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         // vaihda oma opiskelijanumerosi seuraavaan, ÄLÄ kuitenkaan laita githubiin omaa opiskelijanumeroasi
-        String studentNr = "012345678";
+        String studentNr = "014618239";
         if (args.length > 0) {
             studentNr = args[0];
         }
